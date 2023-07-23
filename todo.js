f5ccba2b2d02a5b87258e42ba1991d3bf766ab0a
faa-wtag <farhana.akter@welldev.io>
2023-07-23 19:02:28 +0600

    rewritten ADD functionality, created independent function for for resetting i/p field

--- todo.js
+++ todo.js
@@ -1,14 +1,29 @@
-let inputs = document.getElementById("inp")
+const inputElement = document.getElementById("todoInput");
+//const todos = document.getElementById("todoList");
+const tasksList = document.getElementById("tasksList"); //ul
+//const text = document.querySelector(".text");
+const addBtn = document.getElementById("add-btn");
 
-let text = document.querySelector(".text")
-
-function Add(){
-
-
-    newitem = document.createElement("ul")
-    newitem.innerHTML = `${inputs.value}`
-    text.appendChild(newitem)
-    inputs.value= ""
-
-
+function handleTodo() {
+  const taskText = inputElement.value;
+  const newTodo = document.createElement("li");
+  newTodo.innerHTML = taskText;
+  tasksList.appendChild(newTodo);
+  resetInputField();
 }
+
+/*
+function handleTodo(inputElement) {
+  const {value}= inputElement; //Gives Undefined o/p
+  const newTodo = document.createElement("li");
+  newTodo.innerHTML = value;
+  tasksList.appendChild(newTodo);
+  resetInputField();
+}
+ */
+
+function resetInputField() {
+  inputElement.value = "";
+}
+
+addBtn.addEventListener("click", handleTodo);
