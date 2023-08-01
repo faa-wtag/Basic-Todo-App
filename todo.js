f9f6dcd61ae561c8478e276ff2f66b4ec5c107b2
faa-wtag <farhana.akter@welldev.io>
2023-08-01 10:02:06 +0600

    Introduces the functionality of edit a todo

--- todo.js
+++ todo.js
@@ -3,18 +3,23 @@ const tasks = document.getElementById("tasksList");
 const addBtn = document.getElementById("add-btn");
 
 let todos = [];
+let editTodoId = null;
 
 function handleTodo() {
   const taskText = inputElement.value;
 
-  const newTodo = {
-    id: Date.now(),
-    value: taskText,
-    doneState: false,
-  };
+  if (editTodoId) {
+    handleTodoUpdate(editTodoId);
+  } else {
+    const newTodo = {
+      id: Date.now(),
+      value: taskText,
+      doneState: false,
+    };
+    todos.push(newTodo);
+    renderTodoList();
+  }
 
-  todos.push(newTodo);
-  renderTodoList();
   resetInputField();
 }
 
@@ -27,10 +32,24 @@ function renderTodoList() {
 
   todos.forEach((todo) => {
     const newTodoItem = document.createElement("li");
-    newTodoItem.innerHTML = todo.value;
 
-    const deleteButtonElement = createDeleteButton(todo.id);
-    newTodoItem.appendChild(deleteButtonElement);
+    if (editTodoId === todo.id) {
+      const inputBox = document.createElement("input");
+      inputBox.value = todo.value;
+      const updateButtonElement = createUpdateButton();
+      const cancelButtonElement = createCancelButton();
+
+      newTodoItem.appendChild(inputBox);
+      newTodoItem.appendChild(updateButtonElement);
+      newTodoItem.appendChild(cancelButtonElement);
+    } else {
+      newTodoItem.innerHTML = todo.value;
+
+      const deleteButtonElement = createDeleteButton(todo.id);
+      const editButtonElement = createEditButton(todo.id);
+      newTodoItem.appendChild(deleteButtonElement);
+      newTodoItem.appendChild(editButtonElement);
+    }
     tasks.appendChild(newTodoItem);
   });
 }
@@ -42,6 +61,31 @@ function createDeleteButton(todoId) {
   return deleteButtonElement;
 }
 
+function createEditButton(todoId) {
+  const editButtonElement = document.createElement("button");
+  editButtonElement.innerHTML = "Edit";
+  editButtonElement.addEventListener("click", function () {
+    handleTodoEdit(todoId);
+  });
+  return editButtonElement;
+}
+
+function createUpdateButton() {
+  const updateButtonElement = document.createElement("button");
+  updateButtonElement.innerHTML = "Update";
+  updateButtonElement.addEventListener("click", () =>
+    handleTodoUpdate(editTodoId)
+  );
+  return updateButtonElement;
+}
+
+function createCancelButton() {
+  const cancelButtonElement = document.createElement("button");
+  cancelButtonElement.innerHTML = "Cancel";
+  cancelButtonElement.addEventListener("click", () => cancelEdit());
+  return cancelButtonElement;
+}
+
 function handleTodoDelete(todoId) {
   deleteTodoItem(todoId);
   renderTodoList();
@@ -51,6 +95,35 @@ function deleteTodoItem(todoId) {
   todos = todos.filter((todo) => todo.id !== todoId);
 }
 
-addBtn.addEventListener("click", handleTodo);
+function editTodoItem(todoId) {
+  editTodoId = todoId;
+  renderTodoList();
+}
 
+function updateTodoItem(todoId, editedText) {
+  todos = todos.map((todo) => {
+    if (todo.id === todoId) {
+      return { ...todo, value: editedText };
+    }
+    return todo;
+  });
+  editTodoId = null;
+  renderTodoList();
+}
+
+function cancelEdit() {
+  editTodoId = null;
+  renderTodoList();
+}
+
+function handleTodoEdit(todoId) {
+  editTodoItem(todoId);
+}
+
+function handleTodoUpdate(todoId) {
+  const editedText = document.querySelector("li input").value;
+  updateTodoItem(todoId, editedText);
+}
+
+addBtn.addEventListener("click", handleTodo);
 renderTodoList(); // Initial Rendering of TodoList
